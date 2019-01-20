337d1ecbcf439eda5875a970ebc26805e9188767
OuraganKun <46091068+OuraganKun@users.noreply.github.com>
2019-01-20 20:02:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Discord = ('discord.js');
 const bot = new Discord.Client();
 const cfg = require('./index.json');
+const token = process.env.token;
 const prefix = ("/");
 
 bot.on('ready', function () {
     console.log("I'm ready to send everybody in hell")
 });
 
-bot.login(cfg.token);
+bot.login(token);
